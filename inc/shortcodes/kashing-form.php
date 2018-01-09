43a4ddea15f8bc440347995a5c601c064a108ea0
Filip <prafgon@gmail.com>
2018-01-09 19:14:12 +0100

    Aktualizacja kashing-form.php
    Dodalem przykladowe zapytanie do API Kashingu w shortcodzie [kashing_form]

--- inc/shortcodes/kashing-form.php
+++ inc/shortcodes/kashing-form.php
@@ -10,13 +10,56 @@ if ( !function_exists( 'kashing_form_shortcode' ) ) {
 
         ob_start(); // We can use HTML directly thanks to this
 
-        ?>
+        // Zapytanie do API
 
-        Tutaj mozna uzywac dowolnie HTML, a takze echo itd
+        echo 'Bla:<br><br><br>';
 
-        <?php
+        $url = 'https://development-backend.kashing.co.uk/transaction/init';
 
-        echo 'Event id: ' . $form_id;
+        $array = array(
+            'transactions' => array(array(
+                'merchantid' => "207",
+                'amount' => "100",
+                'currency' => "GBP",
+                'returnurl' => 'http://veented.com',
+                "ip" => "192.168.0.111",
+                "forwardedip" => "80.177.11.240",
+                "merchanturl" => "shop.test.co.uk",
+                "description" => "test description",
+                "firstname" => "Ten",
+                "lastname" => "Green",
+                "address1" => "Flat 6 Primrose Rise",
+                "city" => "Northampton",
+                "postcode" => "12-123",
+                "country" => "UK",
+                "psign" => "7bda80def2391a02891970db5c5f2c58ccd4596b"
+            ))
+        );
+
+        $body = json_encode( $array );
+
+        print_r( $body ); // Wypisujemy BODY zeby zobaczyc jak wyglada
+
+        $response = wp_remote_post(
+            $url,
+            array(
+                'method' => 'POST',
+                'timeout' => 10,
+                'headers' => array( 'Content-Type' => 'application/json' ),
+                'body' => $body,
+            )
+        );
+
+        if ( is_wp_error( $response ) ) {
+            $error_message = $response->get_error_message();
+            echo "Something went wrong: $error_message";
+        } else {
+            echo 'Response:<pre>';
+            print_r( $response );
+            echo '</pre>';
+        }
+
+        echo 'Event id: ' . $form_id; // takie tam pierdoly
 
         $content = ob_get_contents(); // End content "capture" and store it into a variable.
         ob_end_clean();
